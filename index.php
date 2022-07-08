d439f551355aaa8fe22f8819fda0cf18bfa97be4
reavsoddd <108518571+reavsoddd@users.noreply.github.com>
2022-07-08 15:23:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+   <?php echo '<p>My Id is 64010974030 </p>'; ?> 
+     <?php echo '<p>Update-3st</p>'; ?> 
+     <?php echo '<p>Update-4st</p>'; ?>
+
+ </body>
+</html>

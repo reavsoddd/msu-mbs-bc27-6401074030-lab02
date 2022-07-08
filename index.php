f45a35431cbd3e02af8d703fec272e7a77278cc0
reavsoddd <108518571+reavsoddd@users.noreply.github.com>
2022-07-08 15:50:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
-   <?php echo '<p>My Id is 64010974030 </p>'; ?> 
+   <?php echo '<p>My ID 64010974030 </p>'; ?> 
   <?php echo '<p>My name is thiwa </p>'; ?> 
   <?php echo '<p>Update-1st</p>'; ?>
   <?php echo '<p>Update-2st</p>'; ?>
